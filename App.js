604718c81d90c73783ead902d3e4cb4464d4a0ee
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-10 15:56:33 -0300

    Encerramos a apostila 07, introducao ao React Native. Usamos o Hook setState

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//hooks do React. Eles existem a partir da versão 16.8 do React.
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component{
+  constructor (props){
+    super (props)
+    this.state = {
+      texto: 'Texto Inicial',
+      contador: 0
+    }
+  }
+  render(){
+    return(
+      <View
+        style={styles.container}>
+        <Text>{this.state.contador}</Text>
+        <Button 
+          title="Incrementar"
+          onPress={() => this.setState({
+            contador: this.state.contador + 1
+          })}
+        />
+      </View>
+    )
+  }
 }
+
+
+// export default function App() {
+
+//   const [texto, setTexto] = useState('Texto inicial')
+//   return (
+//     <View style={styles.container}>
+//       <Text>{texto}</Text>
+//       <Button 
+//         title="Alterar texto"
+//         onPress={() => setTexto('Um novo texto aqui')}
+//       />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
